2ea05d728a5795c3478a81e0198e5b191d7743e2
F.D.Castel <fdcastel@gmail.com>
2015-12-30 09:25:32 -0200

    Updates firebird to 2.5.5. Updates scripts to follow new guidelines from chocolatey-ops.
    Signed-off-by: F.D.Castel <fdcastel@gmail.com>

--- firebird/tools/chocolateyUninstall.ps1
+++ firebird/tools/chocolateyUninstall.ps1
@@ -1,4 +1,10 @@
-$here = Split-Path -Parent $MyInvocation.MyCommand.Definition
-$installScript = Join-Path $here '.\chocolateyInstall.ps1'
+$toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$toolsScript = Join-Path $toolsDir "..\..\firebirdclient\tools\firebirdTools.ps1"
 
-& $installScript -Uninstall
+. $toolsScript
+
+$packageName = 'firebird'
+
+Uninstall-Firebird $packageName
+
+Install-FirebirdClient 'firebirdclient'
