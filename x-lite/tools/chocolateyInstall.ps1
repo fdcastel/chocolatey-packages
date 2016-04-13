6e59590e7ed2ed24491eee6978174c763031ec8e
F.D.Castel <fdcastel@gmail.com>
2016-04-12 23:37:54 -0300

    Updates x-lite to 4.9.3.79961.
    Signed-off-by: F.D.Castel <fdcastel@gmail.com>

--- x-lite/tools/chocolateyInstall.ps1
+++ x-lite/tools/chocolateyInstall.ps1
@@ -2,11 +2,11 @@ $packageName = 'x-lite'
 $installerType = 'exe'
 
 $silentArgs = '/VERYSILENT /NORESTART'
-$url = 'http://counterpath.s3.amazonaws.com/downloads/X-Lite_4.9.2_79048.exe'
+$url = 'http://counterpath.s3.amazonaws.com/downloads/X-Lite_4.9.3_79961.exe'
 $validExitCodes = @(0,1)
 
 
-$checksum = '7EB91D07DDF7568B3CDA042BC3119AB7B48CE264'
+$checksum = '2656A68CEDEB413C26B8E3EE5D026D3DD30C4E92'
 $checksumType = 'sha1'
 
 Install-ChocolateyPackage -PackageName "$packageName" `
