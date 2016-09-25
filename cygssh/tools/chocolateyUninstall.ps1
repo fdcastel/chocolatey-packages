9bb37114db140e1d9b2a6ae89664ba21f2131592
F.D.Castel <fdcastel@gmail.com>
2016-09-25 15:36:14 -0300

    Removes cygssh and cygrsync.

--- cygssh/tools/chocolateyUninstall.ps1
+++
@@ -1,6 +0,0 @@
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-
-# Alternative to UnInstall-ChocolateyZipPackage
-#   https://github.com/chocolatey/chocolatey/issues?q=UnInstall-ChocolateyZipPackage
-$zipFolder = Join-Path $toolsDir '.\cygssh-master'
-Remove-Item $zipFolder -Recurse
