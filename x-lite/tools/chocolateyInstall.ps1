6cddc2aec67e7f3371bf4a9391bde102c42af8ee
David Gardiner <david@gardiner.net.au>
2017-08-25 15:29:11 +0930

    Update to 5.0.1

--- x-lite/tools/chocolateyInstall.ps1
+++ x-lite/tools/chocolateyInstall.ps1
@@ -5,10 +5,10 @@ $packageName = 'x-lite'
 $installerType = 'exe'
 
 $silentArgs = '-s'
-$url = 'https://counterpath.s3.amazonaws.com/downloads/X-Lite_4.9.8_84253.exe'
+$url = 'https://counterpath.s3.amazonaws.com/downloads/X-Lite_5.0.1_86895.exe'
 $validExitCodes = @(0)
 
-$checksum = '12D6C33C566FAE275191BBA639B890DC4CF0294A840D2DA69528FC3C1DD137EB'
+$checksum = 'E8B100B63975FD225E481763C5CEDE6EE85D12FEA801693B5C4AB901BC06EBDB'
 $checksumType = 'sha256'
 
 $ahkScript = "$toolsDir\install.ahk"
