b6023204750ff9f24ed15e07a6ce6c284414f860
F.D.Castel <fdcastel@gmail.com>
2022-06-15 21:46:16 -0300

    Upgrades Firebird to 3.0.10.

--- firebird/build.ps1
+++ firebird/build.ps1
@@ -8,10 +8,10 @@ $config = @{
         'installerName' = 'Firebird-4.0.1.2692-0.exe'
     }
     'series30' = @{
-        'version' = '3.0.9'
-        'url' = 'https://github.com/FirebirdSQL/firebird/releases/download/v3.0.9/Firebird-3.0.9.33560_0_Win32.exe'
-        'url64' = 'https://github.com/FirebirdSQL/firebird/releases/download/v3.0.9/Firebird-3.0.9.33560_0_x64.exe'
-        'installerName' = 'Firebird-3.0.9.33560_0.exe'
+        'version' = '3.0.10'
+        'url' = 'https://github.com/FirebirdSQL/firebird/releases/download/v3.0.10/Firebird-3.0.10.33601_0_Win32.exe'
+        'url64' = 'https://github.com/FirebirdSQL/firebird/releases/download/v3.0.10/Firebird-3.0.10.33601_0_x64.exe'
+        'installerName' = 'Firebird-3.0.10.33601_0.exe'
     }
     'series25' = @{
         'version' = '2.5.9'
