f6517b3ad7868cb1f17396984c2dad34e201fe37
ENTXawp <39438957+ENTXawp@users.noreply.github.com>
2024-04-18 14:47:51 +0200

    Update chocolateyinstall.ps1
    New URLs:
    x86
    https://download.microsoft.com/download/f/1/3/f13ce329-0835-44e7-b110-44decd29b0ad/en-US/19.3.3.0/x86/msoledbsql.msi
    x64
    https://download.microsoft.com/download/f/1/3/f13ce329-0835-44e7-b110-44decd29b0ad/en-US/19.3.3.0/x64/msoledbsql.msi
    
    New Hashes:
    x86
    833EA175AF60727E25DFA308CECD96084ACD6351F832993C0B10167B83AE5B60
    x64
    892A31D39481DF71C0C6964B3EDD6FD804CD9326DD04049FACF44C413E423AE1
    
    The update fixes the following 24 CVEs:
    
    CVE-2024-29985
    High
    Apr 8, 2024 6:00 PM
    
    CVE-2024-28927
    High
    Apr 8, 2024 6:00 PM
    
    CVE-2024-28939
    High
    Apr 8, 2024 6:00 PM
    
    CVE-2024-28908
    High
    Apr 8, 2024 6:00 PM
    
    CVE-2024-28944
    High
    Apr 8, 2024 6:00 PM
    
    CVE-2024-28940
    High
    Apr 8, 2024 6:00 PM
    
    CVE-2024-28906
    High
    Apr 8, 2024 6:00 PM
    
    CVE-2024-28909
    High
    Apr 8, 2024 6:00 PM
    
    CVE-2024-28945
    High
    Apr 8, 2024 6:00 PM
    
    CVE-2024-28914
    High
    Apr 8, 2024 6:00 PM
    
    CVE-2024-28913
    High
    Apr 8, 2024 6:00 PM
    
    CVE-2024-29046
    High
    Apr 8, 2024 6:00 PM
    
    CVE-2024-29982
    High
    Apr 8, 2024 6:00 PM
    
    CVE-2024-29983
    High
    Apr 8, 2024 6:00 PM
    
    CVE-2024-29048
    High
    Apr 8, 2024 6:00 PM
    
    CVE-2024-28926
    High
    Apr 8, 2024 6:00 PM
    
    CVE-2024-29044
    High
    Apr 8, 2024 6:00 PM
    
    CVE-2024-29045
    High
    Apr 8, 2024 6:00 PM
    
    CVE-2024-28910
    High
    Apr 8, 2024 6:00 PM
    
    CVE-2024-29984
    High
    Apr 8, 2024 6:00 PM
    
    CVE-2024-28942
    High
    Apr 8, 2024 6:00 PM
    
    CVE-2024-28915
    High
    Apr 8, 2024 6:00 PM
    
    CVE-2024-28912
    High
    Apr 8, 2024 6:00 PM
    
    CVE-2024-28911
    High
    Apr 8, 2024 6:00 PM

--- msoledbsql/tools/chocolateyinstall.ps1
+++ msoledbsql/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url        = 'https://download.microsoft.com/download/f/1/3/f13ce329-0835-44e7-b110-44decd29b0ad/en-US/19.3.2.0/x86/msoledbsql.msi'
-$url64      = 'https://download.microsoft.com/download/f/1/3/f13ce329-0835-44e7-b110-44decd29b0ad/en-US/19.3.2.0/x64/msoledbsql.msi'
+$url        = 'https://download.microsoft.com/download/f/1/3/f13ce329-0835-44e7-b110-44decd29b0ad/en-US/19.3.3.0/x86/msoledbsql.msi'
+$url64      = 'https://download.microsoft.com/download/f/1/3/f13ce329-0835-44e7-b110-44decd29b0ad/en-US/19.3.3.0/x64/msoledbsql.msi'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -13,9 +13,9 @@ $packageArgs = @{
 
   softwareName  = 'Microsoft OLE DB Driver for SQL Server'
 
-  checksum      = '4D3998E1BC6FF76AA7C0C347144046C0E82BD40E2EC965ADCD9FF6DAA9CBD0D7'
+  checksum      = '833EA175AF60727E25DFA308CECD96084ACD6351F832993C0B10167B83AE5B60'
   checksumType  = 'sha256'
-  checksum64    = '8A0B76F9880284333E403DB2272B9F46CF2401E984C8864F77DB1D685E94C2BF'
+  checksum64    = '892A31D39481DF71C0C6964B3EDD6FD804CD9326DD04049FACF44C413E423AE1'
   checksumType64= 'sha256'
 
   # MSI
