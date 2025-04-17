4ebd69436d72de9d5f0490dc3c94e860b934411f
F.D.Castel <fdcastel@gmail.com>
2025-04-17 15:35:35 -0300

    Requests from Chocolatey moderation.

--- opkssh/tools/chocolateyInstall.ps1
+++ opkssh/tools/chocolateyInstall.ps1
@@ -1,4 +1,6 @@
-﻿$packageName = 'opkssh'
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName = 'opkssh'
 $url64 = 'https://github.com/openpubkey/opkssh/releases/download/v0.3.0/opkssh-windows-amd64.exe'
 $checksum64 = 'D864576A0007E9CAD914420A94DB51EA74B3473EA22F88317492AFC0B7B4F895'
 
