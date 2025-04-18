92687b0d96f1741964e92f764ce57ec7c5844e66
F.D.Castel <fdcastel@gmail.com>
2025-04-18 09:26:45 -0300

    Adds qemu-img.

---
+++ qemu-img/tools/chocolateyInstall.ps1
@@ -0,0 +1,10 @@
+﻿$packageName = 'qemu-img'
+$url64 = 'https://cloudbase.it/downloads/qemu-img-win-x64-2_3_0.zip'
+$checksum64 = '8DC1C69D9880919CDAD8C09126A016262D4A9EDF48B87A1EF587914FE4177909'
+
+Install-ChocolateyZipPackage `
+    -PackageName "$packageName" `
+    -UnzipLocation "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" `
+    -Url64bit $url64 `
+    -Checksum64 $checksum64 `
+    -ChecksumType64 SHA256
