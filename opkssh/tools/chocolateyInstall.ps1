1aa4111058b9aa8dace8a42ebab3ece49e7836c0
F.D.Castel <fdcastel@gmail.com>
2025-10-05 22:47:26 -0300

    Fix opkssh installation script.

--- opkssh/tools/chocolateyInstall.ps1
+++ opkssh/tools/chocolateyInstall.ps1
@@ -2,11 +2,9 @@
 
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 
-$packageArgs = @{
-  PackageName    = 'opkssh'
-  FileFullPath   = "$toolsDir\opkssh.exe"
-  Url64bit       = '{{assets.browser_download_url}}'
-  Checksum64     = '{{assets.sha256}}'
-  ChecksumType64 = 'sha256'
-}
-Install-ChocolateyPackage @packageArgs
+Get-ChocolateyWebFile `
+    -PackageName 'opkssh' `
+    -FileFullPath "$toolsDir\opkssh.exe" `
+    -Url64bit '{{assets.browser_download_url}}' `
+    -Checksum64 '{{assets.sha256}}' `
+    -ChecksumType64 SHA256
