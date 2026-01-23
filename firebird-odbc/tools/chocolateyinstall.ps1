8da297cde7a00dabe48231187dbd473c628f5191
F.D.Castel <fdcastel@gmail.com>
2026-01-22 21:46:10 -0300

    Convert firebird-odbc package to ChocoForge.

--- firebird-odbc/tools/chocolateyinstall.ps1
+++ firebird-odbc/tools/chocolateyinstall.ps1
@@ -1,26 +1,32 @@
 ﻿$ErrorActionPreference = 'Stop'
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url        = 'https://sourceforge.net/projects/firebird/files/firebird-ODBC-driver/2.0.5-Release/Firebird_ODBC_2.0.5.156_Win32.exe/download'
-$url64      = 'https://sourceforge.net/projects/firebird/files/firebird-ODBC-driver/2.0.5-Release/Firebird_ODBC_2.0.5.156_x64.exe/download'
+$packageName = 'firebird-odbc'
+$toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+
+# These variables will be replaced during build
+$version = '{{version}}'
+$url64 = '{{assets.x64.browser_download_url}}'
+$url32 = '{{assets.x86.browser_download_url}}{{assets.Win32.browser_download_url}}'
+$checksum64 = '{{assets.x64.sha256}}'
+$checksum32 = '{{assets.x86.sha256}}{{assets.Win32.sha256}}'
+$checksumType = 'sha256'
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
-  fileType      = 'exe'
-  url           = $url
-  url64bit      = $url64
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  fileType       = 'EXE'
+  url            = $url32
+  url64bit       = $url64
 
-  softwareName  = 'Firebird ODBC driver*'
+  silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes = @(0)
 
-  checksum      = '20DDB6E602DDB7B249F000829D919B17BD5C37ADA90CAF8E80EEA29C7D4673DA'
-  checksumType  = 'sha256'
-  checksum64    = 'DFB4F5695813BAD3F9F7B9CDDFD22358413B4CBAA91ADE6AC299E3C5973ED7CC'
-  checksumType64= 'sha256'
+  softwareName   = 'Firebird ODBC driver*'
 
-  # MSI
-  silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
-  validExitCodes= @(0)
+  checksum       = $checksum32
+  checksumType   = $checksumType
+  checksum64     = $checksum64
+  checksumType64 = $checksumType
 }
 
 Install-ChocolateyPackage @packageArgs
