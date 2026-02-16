bb2837e22ac8815ff44cb453684005339081ac46
F.D.Castel <fdcastel@gmail.com>
2026-02-16 01:23:17 -0300

    Convert firebird-odbc to use embedded installers.

--- firebird-odbc/tools/chocolateyinstall.ps1
+++ firebird-odbc/tools/chocolateyinstall.ps1
@@ -4,29 +4,20 @@ $packageName = 'firebird-odbc'
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 
 # These variables will be replaced during build
-$version = '{{version}}'
-$url64 = '{{assets.x64.browser_download_url}}'
-$url32 = '{{assets.x86.browser_download_url}}{{assets.Win32.browser_download_url}}'
-$checksum64 = '{{assets.x64.sha256}}'
-$checksum32 = '{{assets.x86.sha256}}{{assets.Win32.sha256}}'
-$checksumType = 'sha256'
+$file64 = '{{assets.x64.name}}'
+$file32 = '{{assets.x86.name}}{{assets.Win32.name}}'
 
 $packageArgs = @{
   packageName    = $packageName
-  unzipLocation  = $toolsDir
   fileType       = 'EXE'
-  url            = $url32
-  url64bit       = $url64
+
+  file          = "$toolsDir\$file32"
+  file64        = "$toolsDir\$file64"
 
   silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes = @(0)
 
   softwareName   = 'Firebird ODBC driver*'
-
-  checksum       = $checksum32
-  checksumType   = $checksumType
-  checksum64     = $checksum64
-  checksumType64 = $checksumType
 }
 
-Install-ChocolateyPackage @packageArgs
+Install-ChocolateyInstallPackage @packageArgs
