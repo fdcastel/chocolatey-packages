1f513c2e4aa414b43f91c51aa56c7378b6398d00
F.D.Castel <fdcastel@gmail.com>
2026-07-03 01:40:10 -0300

    Upgrades msoledbsql to 19.4.2.

--- msoledbsql/tools/chocolateyinstall.ps1
+++ msoledbsql/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url        = 'https://download.microsoft.com/download/a8711829-f40a-4956-84fc-d08e7f877a06/x86/1033/msoledbsql.msi'
-$url64      = 'https://download.microsoft.com/download/b5865bb8-7bc6-4068-9c1d-fb77c256a865/amd64/1033/msoledbsql.msi'
+$url        = 'https://download.microsoft.com/download/0a09a9e0-e364-4d01-b102-04ddfcf38a7e/x86/1033/msoledbsql.msi'
+$url64      = 'https://download.microsoft.com/download/7bf55274-18ac-4b26-9783-45453a1ab64f/amd64/1033/msoledbsql.msi'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -13,9 +13,9 @@ $packageArgs = @{
 
   softwareName  = 'Microsoft OLE DB Driver for SQL Server'
 
-  checksum      = 'C5427A3314A1118FA435BB644AE555BD2B7301E28CF26CB4E0F55DA6823E02E9'
+  checksum      = 'B86F1EE532E6EA543721747EB03B32E5EFF6C292458DE3D90391B97908C8E13C'
   checksumType  = 'sha256'
-  checksum64    = '5ABB621F09B947D5799B90C98159EC6E157734251565F1133EFAED2BDCBF2B1B'
+  checksum64    = '409ADFD93165DD3622B2D7CD0B9C4D96A27B04F9F3FB5599D99ACBE90ADE0638'
   checksumType64= 'sha256'
 
   # MSI
